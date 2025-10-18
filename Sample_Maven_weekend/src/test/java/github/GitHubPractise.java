64925c7f514ff72732262c0a65135b757b543b28
AbhijithTyss <abhijith.styss@gmail.com>
2025-10-18 11:22:59 +0530

    added github package

---
+++ Sample_Maven_weekend/src/test/java/github/GitHubPractise.java
@@ -0,0 +1,12 @@
+package github;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GitHubPractise {
+	@Test
+	public void test() {
+		WebDriver driver=new ChromeDriver();
+	}
+}
